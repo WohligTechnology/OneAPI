29cf318fd5805c431304e27938d86c20f04d26f4
vigkast <vigwohlig@gmail.com>
2016-04-19 18:55:46 +0530

    change

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -8,7 +8,7 @@ module.exports = {
     myFynx: function(req, res) {
         var options = {
             siteType: 'url',
-            renderDelay: 3000,
+            renderDelay: 10000,
             windowSize: {
                 width: 975,
                 height: 1500
