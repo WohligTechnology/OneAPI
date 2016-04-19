bd0df91e4aecf0ed9b0dee2e68697feed6500832
vigkast <vigwohlig@gmail.com>
2016-04-19 11:46:21 +0530

    first commit

---
+++ api/controllers/ImageController.js
@@ -0,0 +1,24 @@
+/**
+ * ImageController
+ *
+ * @description :: Server-side logic for managing Images
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+var webshot = require('webshot');
+module.exports = {
+    myFynx: function(req, res) {
+        var options = {
+            siteType: 'url',
+            renderDelay: 3000,
+            windowSize: {
+                width: 975,
+                height: 1500
+            }
+        };
+        var renderStream = webshot("http://admin.myfynx.com/index2.php/#/" + req.query.url, options);
+        renderStream.on('data', function(data) {
+            res.set('Content-Type', "image/jpeg");
+            res.send(data);
+        });
+    }
+};
