ebc61df7f05cc0f8cc6528efad2142402ee04919
vigkast <vigwohlig@gmail.com>
2016-04-20 11:21:36 +0530

    cleared_properly

--- api/controllers/ImageController.js
+++ api/controllers/ImageController.js
@@ -14,9 +14,31 @@ module.exports = {
                 height: 1500
             }
         };
-        var renderStream = sails.webshot("http://admin.myfynx.com/index2.php/#/" + req.query.url, options);
-        renderStream.on('data', function(data) {
-            res.send(data);
-        });
+        var split = req.query.url.split("/");
+        var path = "./uploads/" + split[1] + ".jpg";
+        var isfile = sails.fs.existsSync(path);
+        if (isfile == false) {
+            console.log("in if");
+            sails.webshot("http://admin.myfynx.com/index2.php/#/" + req.query.url, path, options, function(err) {
+                if (err) {
+                    console.log(err);
+                    res.json({
+                        value: false,
+                        comment: err
+                    });
+                } else {
+                    var image = sails.fs.readFileSync(path);
+                    res.set('Content-Type', "application/octet-stream");
+                    res.set('Content-Disposition', "attachment;filename=" + path);
+                    res.send(image);
+                }
+            });
+        } else {
+            console.log("in else");
+            var image = sails.fs.readFileSync(path);
+            res.set('Content-Type', "application/octet-stream");
+            res.set('Content-Disposition', "attachment;filename=" + path);
+            res.send(image);
+        }
     }
 };
